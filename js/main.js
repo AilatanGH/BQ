a346423657c3ed9614b99f45392c4413a86ec7f7
natalia <ailatangh@gmail.com>
2017-09-18 12:55:52 +0200

    added function js for menu navigation a modified js subnav categories

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@
 
 var menuSmall= document.getElementById("menu-small");
 var btnSubnav = document.getElementById("btn-subnav");
+var btnMenu = document.getElementById("btn-menu");
 var subNav = document.getElementById("subnav");
+var navTabs = document.getElementById("nav-tabs");
 var subDataIcon = document.getElementById("subnav-img");
 
-//Mostrar y ocultar submenú de datos en versión móvil
+// SHOW MENU NAVEGATION
+
+function openCloseNavMenu(){
+	navTabs.classList.toggle("nav-show");
+}
+
+btnMenu.addEventListener("click",openCloseNavMenu);
+
+
+// SHOW MENU CATEGORIES
+
 function openCloseSubNavData(){
 	subNav.classList.toggle("subnav-show");
 	subDataIcon.classList.toggle("subnav-img-rotate");
